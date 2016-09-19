9183c1b8c4226e3f9a5dcc6dfbd5bf04515d8a67
ruslan.babich <ruslan.babich@innoforce.kz>
2016-09-19 12:06:50 +0600

    master: netty client isnt blocking thread now

--- src/main/java/sample/network/NetworkClient.java
+++ src/main/java/sample/network/NetworkClient.java
@@ -1,10 +1,8 @@
 package sample.network;
 
+import io.netty.channel.*;
 import sample.game.*;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -16,7 +14,6 @@ import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 import javax.net.ssl.SSLException;
-import java.io.IOException;
 
 /**
  * Created by ruslan.babich on 16.08.2016.
@@ -24,6 +21,9 @@ import java.io.IOException;
 public class NetworkClient {
     private static final int TCP_PORT = 27015;
     private static final boolean SSL = System.getProperty("ssl") != null;
+    private static Channel channel;
+    private static ChannelFuture channelFuture;
+    private static EventLoopGroup group = new NioEventLoopGroup();
 
     public static void sendChatMessage(String chatMessage) {
         NetworkMessage message = new NetworkMessage();
@@ -50,30 +50,33 @@ public class NetworkClient {
             sslCtx = null;
         }
 
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ChannelPipeline p = ch.pipeline();
-                            if (sslCtx != null) {
-                                p.addLast(sslCtx.newHandler(ch.alloc(), hostIP, TCP_PORT));
-                            }
-                            p.addLast(
-                                    new ObjectEncoder(),
-                                    new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
-                                    new NetworkClientHandler());
+        Bootstrap b = new Bootstrap();
+        b.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        if (sslCtx != null) {
+                            p.addLast(sslCtx.newHandler(ch.alloc(), hostIP, TCP_PORT));
                         }
-                    });
+                        p.addLast(
+                                new ObjectEncoder(),
+                                new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
+                                new NetworkClientHandler());
+                    }
+                });
 
-            // Start the connection attempt.
-            b.connect(hostIP, TCP_PORT).sync().channel().closeFuture().sync();
-        } finally {
-            group.shutdownGracefully();
-        }
+        // Start the connection attempt.
+        channel = b.connect(hostIP, TCP_PORT).sync().channel();
+
         return null;
+    }
+
+    public static void stop() throws InterruptedException {
+        channel.closeFuture().sync();
+        if (channelFuture != null)
+            channelFuture.sync();
+        group.shutdownGracefully();
     }
 }
